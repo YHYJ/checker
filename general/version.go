9ef69a76f9fdb93fbf74a3bf8df50c6d7fb6295a
YJ-ArchLinux-Laptop <yj1516268@outlook.com>
2023-11-01 12:51:56 +0800

    version信息更新

--- general/version.go
+++ general/version.go
@@ -4,7 +4,7 @@ Author: YJ
 Email: yj1516268@outlook.com
 Created Time: 2023-02-27 12:50:26
 
-Description: 子命令`version`功能函数
+Description: 子命令`version`功能实现
 */
 
 package general
@@ -13,22 +13,22 @@ import "fmt"
 
 // 程序信息
 const (
-	name    = "Checker"
-	version = "v0.4.1"
-	project = "github.com/yhyj/checker"
+	Name    string = "Checker"
+	Version string = "v0.4.1"
+	Project string = "github.com/yhyj/checker"
 )
 
 // 编译信息
 var (
-	gitCommitHash string = "unknown"
-	buildTime     string = "unknown"
-	buildBy       string = "unknown"
+	GitCommitHash string = "unknown"
+	BuildTime     string = "unknown"
+	BuildBy       string = "unknown"
 )
 
 func ProgramInfo(only bool) string {
-	programInfo := fmt.Sprintf("%s\n", version)
+	programInfo := fmt.Sprintf("%s\n", Version)
 	if !only {
-		programInfo = fmt.Sprintf("%s version: %s\nGit commit hash: %s\nBuilt on: %s\nBuilt by: %s\n", name, version, gitCommitHash, buildTime, buildBy)
+		programInfo = fmt.Sprintf("%s version: %s\nGit commit hash: %s\nBuilt on: %s\nBuilt by: %s\n", Name, Version, GitCommitHash, BuildTime, BuildBy)
 	}
 	return programInfo
 }
